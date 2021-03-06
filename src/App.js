02a4db58f5036d0bbbccd42936268f88275c416a
hangmoon98 <lethithuyhang@borderless.ws>
2021-03-06 16:43:03 +0200

    API call

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import './App.css'
 import { Card, Chart, CountryPicker } from './components'
 
 import styles from './App.module.css'
+import { fetchData } from './api'
 
+//Use class for App because of asynchronous data
 class App extends React.Component {
+  state = {
+    data: {}
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData()
+
+    this.setState({ data: fetchedData})
+  }
+
   render() {
+    const { data } = this.state
     return (
       <div className={styles.container}>   
-         <Card />
+         <Card data={data} />
          <Chart />
          <CountryPicker />
       </div>
