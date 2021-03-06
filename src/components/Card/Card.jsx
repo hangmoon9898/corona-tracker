e5a1f100e82860973598d28e76c99dce5afbc734
hangmoon98 <lethithuyhang@borderless.ws>
2021-03-06 16:42:25 +0200

    Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import {Grid} from '@material-ui/core'
+import styles from './Card.module.css'
+import SingleCard from './components/SingleCard'
 
-const Cards = () => {
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+    if (!confirmed) {
+        return 'Loading..'
+    }
+
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container justify='center' spacing={3} >
+                <SingleCard 
+                    className={styles.infected}
+                    title='Infected'
+                    number={confirmed}
+                    lastUpdate={lastUpdate}
+                />
+                <SingleCard 
+                    className={styles.recovered}
+                    title='Recovered'
+                    number={recovered}
+                    lastUpdate={lastUpdate}
+                /> 
+                <SingleCard 
+                    className={styles.deaths}
+                    title='Deaths'
+                    number={deaths}
+                    lastUpdate={lastUpdate}
+                /> 
+            </Grid>
+        </div>
     )
 }
 
---
+++ src/components/Card/components/SingleCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Card, CardContent, Typography, Grid} from '@material-ui/core'
+import CountUp from 'react-countup'
+import styles from './SingleCard.module.css'
+import cx from 'classnames'
+
+
+const SingleCard = (props) => {
+    return (
+        <Grid item xs={12} md={3} component={Card} className={cx(styles.card, props.className)}>
+            <CardContent>
+                <Typography color='textSecondary' gutterBottom>{props.title}</Typography>
+                <Typography variant='h5'><CountUp start={0} end={props.number.value} duration={2} separator=' '/></Typography>
+                <Typography color='textSecondary' gutterBottom>{new Date(props.lastUpdate).toDateString()}</Typography>
+            </CardContent>
+        </Grid>
+    )
+}
+
+export default SingleCard
