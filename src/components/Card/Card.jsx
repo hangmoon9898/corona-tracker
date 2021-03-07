4cbd95d70b82ac219cc61610db0853b151b6bfaa
hangmoon98 <lethithuyhang@borderless.ws>
2021-03-07 22:23:58 +0200

    Add Active number to Cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,37 +1,45 @@
-import React from 'react'
-import {Grid} from '@material-ui/core'
-import styles from './Card.module.css'
-import SingleCard from './components/SingleCard'
+import React from "react";
+import { Grid } from "@material-ui/core";
+import styles from "./Card.module.css";
+import SingleCard from "./components/SingleCard";
 
-const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
-    if (!confirmed) {
-        return 'Loading..'
-    }
+const Cards = ({
+  data: { confirmed, recovered, deaths, lastUpdate, active },
+}) => {
+  if (!confirmed) {
+    return "Loading..";
+  }
 
-    return (
-        <div className={styles.container}>
-            <Grid container justify='center' spacing={3} >
-                <SingleCard 
-                    className={styles.infected}
-                    title='Infected'
-                    number={confirmed}
-                    lastUpdate={lastUpdate}
-                />
-                <SingleCard 
-                    className={styles.recovered}
-                    title='Recovered'
-                    number={recovered}
-                    lastUpdate={lastUpdate}
-                /> 
-                <SingleCard 
-                    className={styles.deaths}
-                    title='Deaths'
-                    number={deaths}
-                    lastUpdate={lastUpdate}
-                /> 
-            </Grid>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <Grid container justify="center" spacing={3}>
+        <SingleCard
+          className={styles.infected}
+          title="Infected"
+          number={confirmed}
+          lastUpdate={lastUpdate}
+        />
+        <SingleCard
+          className={styles.recovered}
+          title="Recovered"
+          number={recovered}
+          lastUpdate={lastUpdate}
+        />
+        <SingleCard
+          className={styles.active}
+          title="Active"
+          number={active}
+          lastUpdate={lastUpdate}
+        />
+        <SingleCard
+          className={styles.deaths}
+          title="Deaths"
+          number={deaths}
+          lastUpdate={lastUpdate}
+        />
+      </Grid>
+    </div>
+  );
+};
 
-export default Cards
+export default Cards;
